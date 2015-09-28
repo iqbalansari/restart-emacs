616de40b9f80dc76dd23b5d47d11752a18c4d2f1
Iqbal Ansari <iqbal.ansari@ignitesol.com>
2015-09-28 12:08:11 +0530

    Partially revert "Prefix all symbols with `resem-`, add version and url header"
    Restore `restart-emacs` as the prefix

--- restart-emacs.el
+++ restart-emacs.el
@@ -26,68 +26,68 @@
 
 ;;; Code:
 
-(defvar resem--args nil
+(defvar restart-emacs--args nil
   "The arguments with which to restart Emacs is bound dynamically.")
 
-(defun resem--string-join (strings &optional separator)
+(defun restart-emacs--string-join (strings &optional separator)
   "Join all STRINGS using SEPARATOR."
   (mapconcat 'identity strings separator))
 
-(defun resem--get-emacs-binary ()
+(defun restart-emacs--get-emacs-binary ()
   "Get absolute path to binary of currently running Emacs."
   (expand-file-name invocation-name invocation-directory))
 
-(defun resem--start-gui-using-sh (&optional args)
+(defun restart-emacs--start-gui-using-sh (&optional args)
   "Start GUI version of Emacs using sh.
 
 ARGS is the list arguments with which Emacs should be started"
   (call-process "sh" nil
                 nil nil
                 "-c" (format "%s %s &"
-                             (shell-quote-argument (resem--get-emacs-binary))
-                             (resem--string-join (mapcar #'shell-quote-argument
+                             (shell-quote-argument (restart-emacs--get-emacs-binary))
+                             (restart-emacs--string-join (mapcar #'shell-quote-argument
                                                                  args)
                                                          " "))))
 
-(defun resem--start-gui-on-windows (&optional args)
+(defun restart-emacs--start-gui-on-windows (&optional args)
   "Start GUI version of Emacs on windows.
 
 ARGS is the list arguments with which Emacs should be started"
-  (w32-shell-execute "open" (resem--get-emacs-binary) args))
+  (w32-shell-execute "open" (restart-emacs--get-emacs-binary) args))
 
-(defun resem--start-emacs-in-terminal (&optional args)
+(defun restart-emacs--start-emacs-in-terminal (&optional args)
   "Start Emacs in current terminal.
 
 ARGS is the list arguments with which Emacs should be started.  This requires a
 shell with `fg' command and `;' construct.  This has been tested to work with
 sh, bash, zsh, fish, csh and tcsh shells"
   (suspend-emacs (format "fg ; %s %s -nw"
-                         (shell-quote-argument (resem--get-emacs-binary))
-                         (resem--string-join (mapcar #'shell-quote-argument
+                         (shell-quote-argument (restart-emacs--get-emacs-binary))
+                         (restart-emacs--string-join (mapcar #'shell-quote-argument
                                                              args)
                                                      " "))))
 
-(defun resem--ensure-can-restart ()
+(defun restart-emacs--ensure-can-restart ()
   "Ensure we can restart Emacs on current platform."
   (when (and (not (display-graphic-p))
              (memq system-type '(windows-nt ms-dos)))
     (user-error (format "Cannot restart emacs running in terminal on system of type `%s'" system-type))))
 
-(defun resem--launch-other-emacs ()
+(defun restart-emacs--launch-other-emacs ()
   "Launch another Emacs session according to current platform."
   (apply (if (display-graphic-p)
              (if (memq system-type '(windows-nt msdos))
-                 #'resem--start-gui-on-windows
-               #'resem--start-gui-using-sh)
+                 #'restart-emacs--start-gui-on-windows
+               #'restart-emacs--start-gui-using-sh)
            (if (memq system-type '(windows-nt msdos))
                ;; This should not happen since we check this before triggering a restart
                (user-error "Cannot restart Emacs running in a windows terminal")
-             #'resem--start-emacs-in-terminal))
+             #'restart-emacs--start-emacs-in-terminal))
          ;; Since this function is called in `kill-emacs-hook' it cannot accept
          ;; direct arguments the arguments are let-bound instead
-         (list resem--args)))
+         (list restart-emacs--args)))
 
-(defun resem--translate-prefix-to-args (prefix)
+(defun restart-emacs--translate-prefix-to-args (prefix)
   "Translate the given PREFIX to arguments to be passed to Emacs.
 
 It does the following translation
@@ -100,7 +100,7 @@ It does the following translation
                                             " "))))
 
 ;;;###autoload
-(defun resem-restart-emacs (&optional args)
+(defun restart-emacs (&optional args)
   "Restart Emacs.
 
 When called interactively ARGS is interpreted as follows
@@ -116,13 +116,13 @@ When called non-interactively ARGS should be a list of arguments
 with which Emacs should be restarted."
   (interactive "P")
   ;; Do not trigger a restart unless we are sure, we can restart emacs
-  (resem--ensure-can-restart)
+  (restart-emacs--ensure-can-restart)
   ;; We need the new emacs to be spawned after all kill-emacs-hooks
   ;; have been processed and there is nothing interesting left
-  (let ((kill-emacs-hook (append kill-emacs-hook (list #'resem--launch-other-emacs)))
-        (resem--args (if (called-interactively-p 'any)
-                         (resem--translate-prefix-to-args args)
-                       args)))
+  (let ((kill-emacs-hook (append kill-emacs-hook (list #'restart-emacs--launch-other-emacs)))
+        (restart-emacs--args (if (called-interactively-p 'any)
+                                        (restart-emacs--translate-prefix-to-args args)
+                                      args)))
     (save-buffers-kill-emacs)))
 
 (provide 'restart-emacs)
