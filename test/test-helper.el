fcec0c552510c98395527630ecfe10071d8e1060
Stefan Kangas <stefan@marxist.se>
2022-01-08 10:46:49 +0100

    Copyright and license header fixes

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,7 +1,22 @@
 ;;; test-helper.el --- Tests for restart-emacs             -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2015-2022  Iqbal Ansari
+
 ;;; Commentary:
 ;;; Helpers to write tests for restart-emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Code:
 
